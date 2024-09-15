1da6570d9b17869988a39c644f28796b9db8d4c8
kumilao1 <derrickkumi54@gmail.com>
2024-09-15 08:43:05 -0700

    api

--- api_project/api_project/api/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
-from django.urls import path, include
-
-router = DefaultRouter()
-router.register(r'books', BookViewSet)
-
-urlpatterns = router.urls
