b07b28019728308bd288f745beeddb9af067d4f4
kumilao1 <derrickkumi54@gmail.com>
2024-09-15 09:06:47 -0700

    api

--- advanced-api-project/advanced_api_project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/advanced_api_project/urls.py
@@ -20,3 +20,12 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.contrib import admin
+from django.urls import path
+from api.views import test_api
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/test/', test_api),
+]
+
